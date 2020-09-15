90e2e04b410af62b12b4d54a1025909fb223096b
Jonathan East <jonathan_east@ymail.com>
2020-09-15 21:06:28 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,16 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  
+  validates :is_clickbaity?
   CUSTOM_VALIDATION = [
       /Won't Believe/i,
       /Secret/i,
       /Top [0-9]*/i,
       /Guess/i 
     ]
+    
+  def is_clickbaity?
+    
+  end
+    
 end
